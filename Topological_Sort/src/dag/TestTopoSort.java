0dc0140d05b47c1b0d2951e6a2a66a6de9ec3762
chittalpatel <55499811+chittalpatel@users.noreply.github.com>
2019-11-09 13:23:44 +0530

    Added TestCases file and Made the program to read input from TestCase file

--- Topological_Sort/src/dag/TestTopoSort.java
+++ Topological_Sort/src/dag/TestTopoSort.java
@@ -1,22 +1,55 @@
 package dag;
 
+import java.io.*;
+
 public class TestTopoSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		UGraph g=new UGraph(12);
-		g.addEdge(1, 8);
-		g.addEdge(2, 1);
-		g.addEdge(3, 1);
-		g.addEdge(3, 2);
-		g.addEdge(4, 3);
-		g.addEdge(5, 4);
-		g.addEdge(5, 7);
-		g.addEdge(6, 4);
-		g.addEdge(6, 5);
-		g.addEdge(6, 8);
-		g.addEdge(9, 10);
+	public static void main(String[] args) throws IOException{
+		
+		//Enter the testcase file name(format:"testcase1.txt") here......
+		String testcasefilename="ExampleTestCase.txt";
+		
+		File f=new File("..\\TestCases\\"+testcasefilename);
+		BufferedReader br;
+		try {
+		br=new BufferedReader(new FileReader(f));
+		}catch(FileNotFoundException e) {	
+			System.out.println("File you Enter was Not found Please Enter Valid Filename in \"Filename.txt\" format");	
+			return;
+		}
+		int noOfVertices;
+		try {
+		noOfVertices=Integer.parseInt(br.readLine());
+		}catch(IOException e) {
+			System.out.println("An IOException Occured in noOfVertices Refer the README File");
+			return;
+		}
+		UGraph g=new UGraph(noOfVertices);
+		
+		a:	while(true) {
+			int u,v;
+			String input[];
+			try {
+			input=br.readLine().split(" ");
+			}catch(IOException e) {
+				System.out.println("An IOException Occured in inputting vertex u v (u->v)... Refer the README File");
+				return;
+			}catch(NullPointerException e) { 
+	//When this Exception Occurs it means that there are No further Lines to read and we have taken all input edge pairs u->v 
+				break a;
+			}
+			u=Integer.parseInt(input[0]);
+			v=Integer.parseInt(input[1]);
+			g.addEdge(u, v);
+		}
+		
 		g.topologicalSort();
+		
+		try {
+			br.close();
+			}catch(IOException e) {
+				System.out.println("An Exception Occured while closing the file");
+			}
 	}
 
 }
